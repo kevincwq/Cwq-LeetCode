40f6ce9ca7763a4c826f048443818555a70fdcb9
Peter Chen <peter_chen@trimble.com>
2020-09-23 21:50:54 -0600

    N0007

---
+++ CSharpImpl.Tests/UT_N0007_ReverseInteger.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace CSharpImpl.Tests
+{
+    public class UT_N0007_ReverseInteger
+    {
+        static object[] TestCases =
+        {
+            new object[]{ 123, 321 },
+            new object[]{ -123, -321 },
+            new object[]{ 120, 21 },
+            new object[]{ int.MinValue, 0 },
+            new object[]{ int.MaxValue, 0}
+        };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Solution1(int x, int exptected)
+        {
+            var so = new N0007_ReverseInteger.Solution1();
+            var ans = so.Reverse(x);
+            Assert.AreEqual(exptected, ans);
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Solution2(int x, int exptected)
+        {
+            var so = new N0007_ReverseInteger.Solution2();
+            var ans = so.Reverse(x);
+            Assert.AreEqual(exptected, ans);
+        }
+    }
+}
---
+++ CSharpImpl/N0007_ReverseInteger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpImpl
+{
+    public class N0007_ReverseInteger
+    {
+        // 7. Reverse Integer
+        // https://leetcode.com/problems/reverse-integer/
+        //Given a 32-bit signed integer, reverse digits of an integer.
+
+        //Example 1:
+        //Input: 123
+        //Output: 321
+
+        //Example 2:
+        //Input: -123
+        //Output: -321
+
+        //Example 3:
+        //Input: 120
+        //Output: 21
+
+        //Note:
+        //Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31−1].
+        //For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+        /// <summary>
+        /// Reverse string
+        /// </summary>
+        public class Solution1
+        {
+            public int Reverse(int x)
+            {
+                var sign = x > 0;
+                var digits = x.ToString().Trim('-');
+                char[] charArray = digits.ToCharArray();
+                Array.Reverse(charArray);
+                var rev = new string(charArray);
+                if (int.TryParse(rev, out int result))
+                {
+                    return sign ? result : -result;
+                }
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Pop and Push Digits & Check before Overflow
+        /// </summary>
+        public class Solution2
+        {
+            public int Reverse(int x)
+            {
+                int rev = 0;
+                while (x != 0)
+                {
+                    int pop = x % 10;
+                    x /= 10;
+                    if (rev > int.MaxValue / 10 || (rev == int.MaxValue / 10 && pop > 7)) return 0;
+                    if (rev < int.MinValue / 10 || (rev == int.MinValue / 10 && pop < -8)) return 0;
+                    rev = rev * 10 + pop;
+                }
+                return rev;
+            }
+        }
+    }
+}
