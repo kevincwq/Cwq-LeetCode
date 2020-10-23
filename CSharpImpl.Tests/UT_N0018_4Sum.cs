8aca9760e163164c91b730407188710412879a65
Peter Chen <peter_chen@trimble.com>
2020-10-23 16:22:57 -0600

    N0018

--- CSharpImpl.Tests/UT_N0018_4Sum.cs
+++ CSharpImpl.Tests/UT_N0018_4Sum.cs
@@ -5,17 +5,20 @@ using System.Linq;
 
 namespace CSharpImpl.Tests
 {
-    public class UT_N0018_4Sum 
+    public class UT_N0018_4Sum
     {
         static object[] TestCases =
         {
-            new object[]{ new int[] {}, new int[] { } },
+            new object[]{ new int[] { 1, 0, -1, 0, -2, 2 }, 0, new List<IList<int>>() { new List<int> { -2, -1, 1, 2 }, new List<int> { -2, 0, 0, 2 }, new List<int> { -1, 0, 0, 1 } } },
+            new object[]{ new int[] {}, 0, new List<IList<int>>() { } }
         };
 
-        // [TestCaseSource(nameof(TestCases))]
-        public void Solution1(int[] input, int[] expected)
+        [TestCaseSource(nameof(TestCases))]
+        public void Solution1(int[] input, int target, IList<IList<int>> expected)
         {
-            throw new NotImplementedException();
+            var so = new N0018_4Sum.Solution1();
+            var ans = so.FourSum(input, target);
+            TestHelper.AreEqual(expected, ans);
         }
     }
 }
--- CSharpImpl/N0018_4Sum.cs
+++ CSharpImpl/N0018_4Sum.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpImpl
 {
@@ -26,9 +27,53 @@ namespace CSharpImpl
     /// </summary>
     public class N0018_4Sum
     {
-        public class Solution1 {
-            public IList<IList<int>> FourSum(int[] nums, int target) {
-                throw new NotImplementedException();
+        public class Solution1
+        {
+            public IList<IList<int>> FourSum(int[] nums, int target)
+            {
+                Array.Sort(nums);
+                return NSum(nums, 0, 4, target).ToList();
+            }
+
+            private IEnumerable<IList<int>> NSum(int[] nums, int startIndex, int intergerNum, int target)
+            {
+                if (intergerNum > 2)
+                {
+                    int pre = int.MinValue;
+                    for (int i = startIndex; i < nums.Length - intergerNum + 1; i++)
+                    {
+                        if (nums[i] == pre)
+                            continue;
+                        pre = nums[i];
+                        foreach (var res in NSum(nums, i + 1, intergerNum - 1, target - pre))
+                        {
+                            res.Insert(0, pre);
+                            yield return res;
+                        }
+                    }
+                }
+                else
+                {
+                    int lo = startIndex, hi = nums.Length - 1;
+                    while (lo < hi)
+                    {
+                        int lov = nums[lo], hiv = nums[hi];
+                        if (lov + lov > target || hiv + hiv < target)
+                            break;
+
+                        var sum = lov + hiv;
+                        if (sum < target || (lo > startIndex || lov == nums[lo - 1]))
+                            lo++;
+                        else if (sum > target || (hi < nums.Length - 1 && hiv == nums[hi + 1]))
+                            hi--;
+                        else
+                        {
+                            yield return new List<int> { lov, hiv };
+                            lo++;
+                            hi--;
+                        }
+                    }
+                }
             }
         }
     }
