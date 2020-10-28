13b7de99c9c72cda51f193fad6c031ac33f9d618
Weiqing Chen <kevincwq@gmail.com>
2020-10-27 21:42:25 -0600

    N0031

--- CSharpImpl.Tests/UT_N0031_NextPermutation.cs
+++ CSharpImpl.Tests/UT_N0031_NextPermutation.cs
@@ -5,17 +5,23 @@ using System.Linq;
 
 namespace CSharpImpl.Tests
 {
-    public class UT_N0031_NextPermutation 
+    public class UT_N0031_NextPermutation
     {
         static object[] TestCases =
         {
-            new object[]{ new int[] {}, new int[] { } },
+            new object[]{ new int[] {1, 2, 3}, new int[] {1, 3, 2 } },
+            new object[]{ new int[] {3, 2, 1}, new int[] {1, 2, 3 } },
+            new object[]{ new int[] {1, 1, 5}, new int[] {1, 5, 1 } },
+            new object[]{ new int[] { 1, 2, 3, 4, 5, 4, 8, 9, 2, 3, 3, 4, 5, 9, 0, 1 }, new int[] { 1, 2, 3, 4, 5, 4, 8, 9, 2, 3, 3, 4, 5, 9, 1, 0 } },
+            new object[]{ new int[] {1 }, new int[] {1 } },
         };
 
-        // [TestCaseSource(nameof(TestCases))]
+        [TestCaseSource(nameof(TestCases))]
         public void Solution1(int[] input, int[] expected)
         {
-            throw new NotImplementedException();
+            var so = new N0031_NextPermutation.Solution1();
+            so.NextPermutation(input);
+            Assert.IsTrue(Enumerable.SequenceEqual(expected, input));
         }
     }
 }
--- CSharpImpl/N0031_NextPermutation.cs
+++ CSharpImpl/N0031_NextPermutation.cs
@@ -34,9 +34,43 @@ namespace CSharpImpl
     /// </summary>
     public class N0031_NextPermutation
     {
-        public class Solution1 {
-            public void NextPermutation(int[] nums) {
-                throw new NotImplementedException();
+        public class Solution1
+        {
+            private void Reverse(int[] nums, int start)
+            {
+                int i = start, j = nums.Length - 1;
+                while (i < j)
+                {
+                    Swap(nums, i, j);
+                    i++;
+                    j--;
+                }
+            }
+
+            private void Swap(int[] nums, int i, int j)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+
+            public void NextPermutation(int[] nums)
+            {
+                int i = nums.Length - 2;
+                while (i >= 0 && nums[i + 1] <= nums[i])
+                {
+                    i--;
+                }
+                if (i >= 0)
+                {
+                    int j = nums.Length - 1;
+                    while (j >= 0 && nums[j] <= nums[i])
+                    {
+                        j--;
+                    }
+                    Swap(nums, i, j);
+                }
+                Reverse(nums, i + 1);
             }
         }
     }
