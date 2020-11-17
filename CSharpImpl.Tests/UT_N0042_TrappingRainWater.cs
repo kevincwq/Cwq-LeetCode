4b444f7c48aec0e07135fe5fa514bbeee09e75e8
Peter Chen <peter_chen@trimble.com>
2020-11-16 20:10:07 -0700

    add N0042

--- CSharpImpl.Tests/UT_N0042_TrappingRainWater.cs
+++ CSharpImpl.Tests/UT_N0042_TrappingRainWater.cs
@@ -1,21 +1,31 @@
-using System;
-using System.Collections.Generic;
 using NUnit.Framework;
-using System.Linq;
 
 namespace CSharpImpl.Tests
 {
-    public class UT_N0042_TrappingRainWater 
+    public class UT_N0042_TrappingRainWater
     {
         static object[] TestCases =
         {
-            new object[]{ new int[] {}, new int[] { } },
+            new object[]{ new int[] { 0, 1 }, 0 },
+            new object[]{ new int[] { 0, 1, 0, 2, 0, 1, 0 }, 2 },
+            new object[]{ new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6 },
+            new object[]{ new int[] { 1, 3, 0, 9, 3, 1, 5, 7, 0, 0, 1, 2, 3 }, 24 }
         };
 
-        // [TestCaseSource(nameof(TestCases))]
-        public void Solution1(int[] input, int[] expected)
+        [TestCaseSource(nameof(TestCases))]
+        public void Solution1(int[] input, int expected)
         {
-            throw new NotImplementedException();
+            var so = new N0042_TrappingRainWater.Solution1();
+            var ans = so.Trap(input);
+            Assert.AreEqual(expected, ans);
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Solution2(int[] input, int expected)
+        {
+            var so = new N0042_TrappingRainWater.Solution2();
+            var ans = so.Trap(input);
+            Assert.AreEqual(expected, ans);
         }
     }
 }
--- CSharpImpl/N0042_TrappingRainWater.cs
+++ CSharpImpl/N0042_TrappingRainWater.cs
@@ -13,12 +13,82 @@ namespace CSharpImpl
     /// Input: [0,1,0,2,1,0,1,3,2,1,2,1]
     /// Output: 6
     /// 
+    /// Constraints:
+    /// n == height.length
+    /// 0 <= n <= 3 * 10^4
+    /// 0 <= height[i] <= 10^5
     /// </summary>
     public class N0042_TrappingRainWater
     {
-        public class Solution1 {
-            public int Trap(int[] height) {
-                throw new NotImplementedException();
+        /// <summary>
+        /// Two pointers
+        /// </summary>
+        public class Solution1
+        {
+            public int Trap(int[] height)
+            {
+                int total = 0;
+                if (height.Length > 2)
+                {
+                    int index_l = 1, index_r = height.Length - 2;
+                    int level_l = height[0], level_r = height[height.Length - 1];
+                    while (index_l <= index_r)
+                    {
+                        if (level_l <= level_r)
+                        {
+                            if (height[index_l] < level_l)
+                            {
+                                total += level_l - height[index_l];
+                            }
+                            else if (height[index_l] > level_l)
+                            {
+                                level_l = height[index_l];
+                            }
+                            index_l++;
+                        }
+                        else
+                        {
+                            if (height[index_r] < level_r)
+                            {
+                                total += level_r - height[index_r];
+                            }
+                            else if (height[index_r] > level_r)
+                            {
+                                level_r = height[index_r];
+                            }
+                            index_r--;
+                        }
+                    }
+                }
+                return total;
+            }
+        }
+
+        /// <summary>
+        /// Level by level (obsolete)
+        /// </summary>
+        public class Solution2
+        {
+            public int Trap(int[] height)
+            {
+                int total = 0, level = 0, low = 0, high = height.Length - 1;
+                int subTotal;
+                do
+                {
+                    subTotal = 0;
+                    while (height[low] <= level)
+                        low++;
+                    while (height[high] <= level)
+                        high--;
+                    for (int i = low + 1; i < high; i++)
+                    {
+                        if (height[i] <= level)
+                            subTotal++;
+                    }
+                    total += subTotal;
+                    level++;
+                } while (subTotal > 0);
+                return total;
             }
         }
     }
